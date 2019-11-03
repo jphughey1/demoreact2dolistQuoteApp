9073e6321c8956e1880641b2cd78145f89243b70
whynot1 <jphughey1@gmail.com>
2019-11-03 17:13:55 -0500

    initial commit

---
+++ build/precache-manifest.30938091a7d2c76d6afd2d79162237a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d025df97ee5f4f9ddb87bfacb6c002a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9be79b78f917c5c3711e",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "9be79b78f917c5c3711e",
+    "url": "/static/js/2.45b32069.chunk.js"
+  },
+  {
+    "revision": "3af6e2e460998ce881bc",
+    "url": "/static/js/main.ad16e18f.chunk.js"
+  },
+  {
+    "revision": "97bcfc6e3d245453bb51",
+    "url": "/static/js/runtime-main.e9659ae4.js"
+  }
+]);
